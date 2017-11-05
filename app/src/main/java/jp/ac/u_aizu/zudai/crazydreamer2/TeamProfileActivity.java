5a60f53590ee0fb23327a7feaf8b1e1ab7c03d17
Shotaro Kunii <tarotaro071028@gmail.com>
2017-11-05 18:48:28 +0900

    ユーザープロフィール変更

---
+++ app/src/main/java/jp/ac/u_aizu/zudai/crazydreamer2/TeamProfileActivity.java
@@ -0,0 +1,13 @@
+package jp.ac.u_aizu.zudai.crazydreamer2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TeamProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teamprofile);
+    }
+}
